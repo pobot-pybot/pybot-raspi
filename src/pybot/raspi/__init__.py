05331709075cf23c72db5354983f4a1a295f3b2b
Eric Pascual <eric@pobot.org>
2015-07-04 15:07:49 +0200

    restructured as namespace based packages

---
+++ src/pybot/raspi/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+""" Specific stuff useful when targeting a RaspberyPi
+"""
+
+import os
+
+# make it possible to import this module on non-RasPi platform when generating documentation
+if 'AUTO_DOC' not in os.environ:
+    import platform
+    if not platform.machine().startswith('armv6'):
+        raise ImportError('can be imported on Raspberry Pi only')
+
+    from .i2c import *
+
+else:
+    i2c_bus = None
